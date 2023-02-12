5b4c21a0b6cd12c8e12889d76a8981a5acc6eb2f
heqro <heqromancer@gmail.com>
2023-02-12 13:54:47 +0100

    add test images and improve test

--- _test_im_tools.py
+++ _test_im_tools.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 import numpy as np
 
@@ -31,14 +32,6 @@ class MyTestCase(unittest.TestCase):
         im_y_conv = im_tools.grady(img, gradient_type=GradientType.CENTERED, use_convolution=True)
         assert np.equal(im_y_conv.all(), im_y_no_conv.all())
 
-    def test_fast_kernel_noise_variation(self):
-        img = preprocessing.load_normalized_image('synth_images_testing/synth_img_256_256.png')
-        for i in range(5, 1000, 5):
-            img_noise = preprocessing.add_gaussian_noise(img, 0, i / 100)
-            # noise_estimation = im_tools.fast_noise_std_estimation(img_noise)
-            print(i/100,im_tools.fast_noise_std_estimation(img_noise))
-            # np.testing.assert_approx_equal(noise_estimation, i / 100, significant=1)
-
     def test_tensorflow_grad_x(self):
         img = preprocessing.load_normalized_image('test_images/dali.jpg')
         tf_img = preprocessing.tf_load_normalized_image('test_images/dali.jpg')
@@ -70,6 +63,40 @@ class MyTestCase(unittest.TestCase):
         im_y_conv = im_tools.grady(img, gradient_type=GradientType.FORWARD)
         im_y_conv = np.reshape(im_y_conv, newshape=tf_img.shape)
         assert np.equal(im_y_conv.all(), im_tools.tf_grad_y(tf_img, gradient_type=GradientType.BACKWARD).numpy().all())
+
+    def test_fast_kernel_noise_variation(self):
+        import time
+
+        img_pool = 100
+        std_values = np.arange(0.01, .51, 0.01)
+        tests_amount = img_pool * len(std_values)
+        successes = 0
+
+        img_fail_index = []
+        img_fail_value = []
+        img_fail_estimation = []
+
+        start = time.time()
+        print("Timing started")
+        for j in range(img_pool):
+            img = preprocessing.load_normalized_image("noise_test_images/img_" + str(j))
+            for i in np.arange(0.01, .51, 0.01):
+                img_noise = preprocessing.add_gaussian_noise(img, 0, i)
+                estimation = im_tools.fast_noise_std_estimation(img_noise)
+                if math.isclose(estimation, i, abs_tol=0.01):
+                    successes += 1
+                else:
+                    img_fail_index += [j]
+                    img_fail_value += [i]
+                    img_fail_estimation += [estimation]
+
+        end = time.time()
+        print("Time taken:", end - start)
+        print("We have correctly estimated", successes, "out of", tests_amount, "noisy images, (",
+              100 * successes / tests_amount, "%)")
+        print("Failure results:")
+        for index in range(len(img_fail_index)):
+            print(img_fail_index[index], img_fail_value[index], img_fail_estimation[index])
 
 
 if __name__ == '__main__':
