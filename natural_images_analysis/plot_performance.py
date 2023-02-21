0a88509676ca40d1fb86bea458a036ba2535e281
heqro <heqromancer@gmail.com>
2023-02-21 11:23:09 +0100

    add natural images analysis pdfs

---
+++ natural_images_analysis/plot_performance.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('all_analysis.csv')
+
+max_diff_time = 50
+from matplotlib.backends.backend_pdf import PdfPages
+
+dfs_dict = {}
+for noise in [0.05, 0.10, 0.15]:
+    # delete timed-out simulation results in right noise profile
+    aux_df = df[(df['noise_std'] == noise) & (df['t_diff'] < max_diff_time)]
+    # select relative error columns
+    dfs_dict[str(noise)] = aux_df.filter(regex='^img_index|^rel_error_synth')
+
+noise_get = '0.15'
+for step in range(0, len(dfs_dict.get(noise_get)), 20):
+    img_indices = dfs_dict.get(noise_get)[dfs_dict.get(noise_get).columns[0]][step:(step + 20)]
+    fig = plt.figure(figsize=(15, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    i = 1
+    for col_name in dfs_dict.get(noise_get).columns[1:]:
+        ax.scatter(img_indices, dfs_dict.get(noise_get)[col_name][step:(step + 20)],label=f'syn_{i}')
+        i += 1
+    ax.set_xticks(img_indices)
+
+    plt.title(f'Relative error w.r.t. maximum PSNR value\nstd_noise = {0.15}')
+    plt.xlabel('image indices')
+    plt.legend(loc=(1.04, 0))
+    pp = PdfPages(f'015-{step}.pdf')
+    pp.savefig(fig)
+    pp.close()
+    # plt.show()
+
+# jugadita de pillar las cols q me interesan
+# for col in err_005.columns[1:]:
+#     print(col)
+# jugadita de pillar la col como index
